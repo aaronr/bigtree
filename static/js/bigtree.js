d0b95220c6f65795edf81921f67a0f7c66cf8243
Aaron Racicot <aaronr@z-pulley.com>
2013-02-01 20:38:19 +0000

    go 3 deep and make things smoother

--- static/js/bigtree.js
+++ static/js/bigtree.js
@@ -1,5 +1,36 @@
+function bigtreeOpen(e) {
+    //console.log(e.node);
+    
+    // Loop through all the children, and if they are directories
+    // we should refresh them 3 deep
+    var node = e.node;
+    for (var i=0;i<node.children.length;i++)
+    {
+	var url = "http://bigtree.projfinder.com/api/bigtree?root="+e.node.children[i].path+"&depth=3"
+	url = url + "&callback=?";
+	//$.getJSON(url, function(databack) {
+	//	$('#tree').tree('loadData', databack.data);
+	//});
+	$.getJSON(url,(function(thisnode) {
+	    return function(databack) {
+		var ids = Array();
+		for (var ii=0;ii<thisnode.children.length;ii++)
+		{
+		    ids.push(thisnode.children[ii].id)
+		}
+		for (var iii=0;iii<ids.length;iii++)
+		{
+		    $('#tree').tree('removeNode', $('#tree').tree('getNodeById', ids[iii]));
+		}
+		$('#tree').tree('loadData', databack.data, thisnode);
+		1+1;
+	    };
+	}(node.children[i]))); // calling the function with the current value
+    }
+}
+
 function init(){
-    var url = "http://bigtree.projfinder.com/api/bigtree?root=D0&depth=2"
+    var url = "http://bigtree.projfinder.com/api/bigtree?root=D0&depth=3"
     url = url + "&callback=?";
     $.getJSON(url, function(databack) {
 	// First just 2 levels
@@ -10,29 +41,7 @@ function init(){
 
     $('#tree').bind(
 	'tree.open',
-	function(e) {
-            console.log(e.node);
-	    var node = e.node;
-	    var url = "http://bigtree.projfinder.com/api/bigtree?root="+e.node.path+"&depth=2"
-	    url = url + "&callback=?";
-	    //$.getJSON(url, function(databack) {
-	    //	$('#tree').tree('loadData', databack.data);
-	    //});
-	    $.getJSON(url,(function(thisnode) {
-		return function(databack) {
-		    var ids = Array();
-		    for (var i=0;i<thisnode.children.length;i++)
-		    {
-			ids.push(thisnode.children[i].id)
-		    }
-		    for (var i=0;i<ids.length;i++)
-		    {
-			$('#tree').tree('removeNode', $('#tree').tree('getNodeById', ids[i]));
-		    }
-		    $('#tree').tree('loadData', databack.data, thisnode);
-		};
-	    }(node))); // calling the function with the current value
-	}
+	bigtreeOpen
     );
 
     // Hook up to the button to query
